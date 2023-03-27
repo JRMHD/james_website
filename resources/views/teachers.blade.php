7729f1bd6ee9aef30e93611d3b6d8d843d2d21e1
JRMHD <117481963+JRMHD@users.noreply.github.com>
2023-03-27 16:45:13 +0300

    progress

--- resources/views/teachers.blade.php
+++ resources/views/teachers.blade.php
@@ -4,7 +4,101 @@
     <div class="container">
         <div class="row justify-content-center">
 
-           
+           {{-- create Teachers form --}}
+
+        {{-- <form method="POST" class=" mb-4" action="{{ route('') }}"> --}}
+            @csrf
+            <div class="grid gap-6 mb-6 md:grid-cols-3">
+                <div>
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full
+                        name</label>
+                    <input type="text" id="name" name="name"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Reagan Mukabana" required>
+                </div>
+                {{-- '','','','dob','','paid','','county','gender','next_of_keen','','id_no' --}}
+
+                <div>
+                    <label for="unit"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Unit</label>
+                    <input type="text" id="unit" name="unit"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="ICT DEPARTMENT" required>
+                </div>
+                <div>
+                    <label for="tell" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone
+                        number</label>
+                    <input type="number" id="tell" name="tell"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required>
+                </div>
+               
+
+                <div>
+                    <label for="tribe" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tribe
+                    </label>
+                    <input type="text" id="tribe" name="tribe"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Luhya" required>
+                </div>
+                <div>
+                    <label for="fee" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Salary</label>
+                    <input type="salary" id="fee" name="salary"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="" required>
+                </div>
+                <div>
+                    <label for="dob" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">DOB</label>
+                    <input type="date" id="dob" name="dob"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="16/01/2001" required>
+                </div>
+                <div>
+                    <label for="county"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">County</label>
+                    <input type="text" id="county" name="county"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Nairobi" required>
+                </div>
+                <div>
+                    <label for="gender"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
+                    <input type="text" id="gender" name="gender"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Male/Female" required>
+                </div>
+                <div>
+                    <label for="next_of_keen" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Next Of
+                        Keen</label>
+                    <input type="text" id="next_of_keen" name="next_of_keen"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Shalom Perazim" required>
+                </div>
+                <div>
+                    <label for="id_no" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Id
+                        Number</label>
+                    <input type="number" id="id_no" name="id_no"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="12345678" required>
+                </div>
+            </div>
+
+            <div class="flex items-start mb-6">
+                <div class="flex items-center h-5">
+                    <input id="remember" type="checkbox" value=""
+                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                        required>
+                </div>
+                <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree
+                    with
+                    the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and
+                        conditions</a>.</label>
+            </div>
+            <button type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+        </form>
+
+        {{-- End of the form --}}
 <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-900 uppercase dark:text-gray-400">
